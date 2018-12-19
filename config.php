eaf4a033c5ca58c6928726b511079807bf282e23
akbarnuri <makerctvvip@gmail.com>
2018-12-20 04:23:20 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+///By Akbar
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh4OZrrp9aLO1dtq9o4ejfoailoO5eaezm5NikaC_lIqKgNCuun6gtLV22q2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "fe71cc69ad3df502";
+$os="android";
+$sign="bb3d312bc0b8dc7ded3d3458c2b99b185d0283f9ecb28678f330ed34c9fa65b7";
